5625efbffd4f78a7b358e04deec86b1703768f5e
Pelegrinetti <pelegrinetti.dev@gmail.com>
2021-03-11 00:23:34 -0300

    Create MediaProvider

---
+++ src/MediaProvider/MediaProvider.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+interface IMediaContext {
+  [key: string]: boolean;
+  default?: boolean | null;
+}
+interface Props {
+  patterns: {
+    [key: string]: string;
+  };
+  defaultMatch?: boolean | undefined;
+}
+
+export const MediaContext = React.createContext<IMediaContext>({});
+
+const MediaProvider: React.FC<Props> = ({
+  children,
+  patterns,
+  defaultMatch
+}) => {
+  const matchesWithPatterns: IMediaContext = {
+    default: defaultMatch
+  };
+
+  if (typeof window !== 'undefined') {
+    Object.keys(patterns).forEach((pattern) => {
+      matchesWithPatterns[pattern] = window.matchMedia(
+        patterns[pattern]
+      ).matches;
+    });
+  }
+
+  const memoizedMatches = React.useMemo(() => matchesWithPatterns, [patterns]);
+
+  return (
+    <MediaContext.Provider value={memoizedMatches}>
+      {children}
+    </MediaContext.Provider>
+  );
+};
+
+export default MediaProvider;
